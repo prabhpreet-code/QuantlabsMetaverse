aa885cde58a9abad91ef14164380ecef8b3562b7
AlmazAkylbekovPractice <practiceakylbekov@gmail.com>
2022-09-28 08:25:23 +0600

    Player talk animation added

--- Assets/Characters/Scripts/CharacterMovement.cs
+++ Assets/Characters/Scripts/CharacterMovement.cs
@@ -10,11 +10,12 @@ public class CharacterMovement : MonoBehaviour
 
     //Character properties
     private float _walkSpeed = 4f;
-    private float _jumpHeight = 5f;
+    private float _jumpHeight = 4f;
 
     //Character Physical Properties
     private bool _isGrounded;
     private bool _jumped;
+    private bool _talked;
 
     //Camera variables
     [SerializeField]
@@ -31,6 +32,9 @@ public class CharacterMovement : MonoBehaviour
 
     private string JUMPING_TAG = "isJumping";
     private bool JUMPING_STATE;
+
+    private string TALKING_TAG = "isTalking";
+    private bool TALKING_STATE;
 
     private void Awake()
     {
@@ -55,6 +59,11 @@ public class CharacterMovement : MonoBehaviour
     {
         _playerMoveInput.x = Input.GetAxis("Horizontal");
         _playerMoveInput.z = Input.GetAxis("Vertical");
+
+        _jumped = Input.GetKey("space");
+        if (Input.GetKeyDown("f"))
+            _talked = true;
+            
     }
 
     private void HandleMovement()
@@ -62,18 +71,19 @@ public class CharacterMovement : MonoBehaviour
         if (_playerMoveInput.magnitude > 0)
         {
             MovePlayer();
+            JumpPlayer();
 
         } else
         {
             IdlePlayer();
         }
 
+        TalkPlayer();
         MoveCharacterController();
     }
 
     private void MovePlayer()
     {
-        WALKING_STATE = true;
 
         float targetAngle = Mathf.Atan2(_playerMoveInput.x, _playerMoveInput.z) * Mathf.Rad2Deg + _cam.eulerAngles.y;
 
@@ -81,27 +91,71 @@ public class CharacterMovement : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        _playerDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        if (_controller.isGrounded)
+        {
+            _playerDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _playerDirection *= _walkSpeed;
 
-        _playerDirection *= _walkSpeed;
+            JUMPING_STATE = false;
+            
+        }
 
+        _talked = false;
+        WALKING_STATE = true;
     }
 
     private void IdlePlayer()
     {
-        WALKING_STATE = false;
-        _playerDirection = Vector3.zero;
+        if (!_talked)
+        {
+            _playerDirection.x = 0f;
+            _playerDirection.z = 0f;
+
+            JUMPING_STATE = false;
+            WALKING_STATE = false;
+        }
+        
+    }
+
+    private void TalkPlayer()
+    {
+        if (_talked)
+        {
+            TALKING_STATE = true;
+
+            JUMPING_STATE = false;
+            WALKING_STATE = false;
+
+            _playerDirection.x = 0f;
+            _playerDirection.z = 0f;
+        } else
+        {
+            TALKING_STATE = false;
+        }
+    }
+
+    private void JumpPlayer()
+    {
+        if (_jumped & _controller.isGrounded)
+        {
+            JUMPING_STATE = true;
+
+            _playerDirection.y += _jumpHeight;
+            _jumped = false;
+        }  
     }
 
     private void MoveCharacterController()
     {
-        _playerDirection += Physics.gravity;
+        _playerDirection += Physics.gravity * Time.deltaTime;
         _controller.Move(_playerDirection * Time.deltaTime);
     }
+
 
     private void AnimateCharacter()
     {
         _animator.SetBool(JUMPING_TAG, JUMPING_STATE);
         _animator.SetBool(WALKING_TAG, WALKING_STATE);
+        _animator.SetBool(TALKING_TAG, TALKING_STATE);
     }
 }
