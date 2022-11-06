95e2eaf72a2321fd7c5a4719ab555c5a4054a103
AlmazAkylbekovPractice <practiceakylbekov@gmail.com>
2022-11-06 23:19:27 +0600

    WebGL Build

--- Assets/Scripts/Objects Scripts/ScreenPlayer.cs
+++ Assets/Scripts/Objects Scripts/ScreenPlayer.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Video;
 
 public class ScreenPlayer : MonoBehaviour
 {
-    private GameObject screen;
-    
+    [SerializeField] private VideoClip video;
+
     private void Awake()
     {
         GetComponent<UnityEngine.Video.VideoPlayer>().url = System.IO.Path.Combine(Application.streamingAssetsPath, "SampleVideo.mp4");
     }
+
 }
